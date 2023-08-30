9ccc0d9f290e41d368f7b9b043729b4a9db47adb
kakeshi <prashant2003gupta@gmail.com>
2023-08-30 16:45:46 +0530

    .

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-bx3-f)$+(9t^jsry3-#z1!!v6!74_r+n2msribpo5le80w)czi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["main-bvxea6i-ljcy227rjuih4.us-3.platformsh.site"]
 
 
 # Application definition
